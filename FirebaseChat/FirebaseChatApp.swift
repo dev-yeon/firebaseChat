220ab6195356053fe70178ce8ab03f8a91b7fb38
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-10-07 22:23:17 +0900

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by yeon on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
