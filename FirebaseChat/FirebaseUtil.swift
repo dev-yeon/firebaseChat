e10be0041c524a8aa407816ca574690202122d9a
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-10-10 01:35:48 +0900

    [Add] firebaseUtil

---
+++ FirebaseChat/FirebaseUtil.swift
@@ -0,0 +1,24 @@
+//
+//  FirebaseUtil.swift
+//  FirebaseChat
+//
+//  Created by yeon on 10/10/23.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseManager: NSObject {
+    let auth: Auth
+    //스스로 객체 생성 - 인스턴스 (싱글톤)
+    static let shared = FirebaseManager()
+    
+    //객체생성시 자동호출됨
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        
+        super.init()
+    }
+}
